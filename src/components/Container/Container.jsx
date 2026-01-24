2b86b78c0485ce2004468223b95de2feb0dd2e45
parallel5846 <brg5846@gmail.com>
2026-01-24 12:30:11 +0530

    added some new feature

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -270,7 +270,12 @@ export default function Container({ chatId = 0, onMenuClick }) {
     };
   }, []);
 
-  
+  const suggestedPrompts = [
+    "Explain quantum computing in simple terms",
+    "Got any creative ideas for a 10 year old's birthday?",
+    "How do I make an HTTP request in Javascript?",
+    "What is the capital of Canada?",
+  ];
 
   return (
     <section
